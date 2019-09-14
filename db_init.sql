2063be4b9b872ec40408bc29b59fb167547043c4
Martín Stancanelli <mastanca14@gmail.com>
2019-09-14 18:13:56 -0300

    Format file

--- db_init.sql
+++ db_init.sql
@@ -1,54 +1,50 @@
-create TABLE IF NOT EXISTS plans (
-    plan INTEGER NOT NULL primary key,
+CREATE TABLE IF NOT EXISTS plans (
+    plan INTEGER NOT NULL PRIMARY KEY,
     plan_name VARCHAR(255)
-    );
+);
 
-create table if not EXISTS users (
-    dni integer not null primary key,
-    plan integer references plans(plan),
-    first_name varchar(255),
-    last_name varchar(255),
+CREATE TABLE IF NOT EXISTS users (
+    dni INTEGER NOT NULL PRIMARY KEY,
+    plan INTEGER REFERENCES plans(plan),
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
     password VARCHAR(255),
     blocked BOOLEAN,
     mail VARCHAR(255)
 );
 
-create table if not exists hospitals (
-    id integer not null primary key,
-    minimum_plan integer REFERENCES plans(plan),
+CREATE TABLE IF NOT EXISTS hospitals (
+    id INTEGER NOT NULL PRIMARY KEY,
+    minimum_plan INTEGER REFERENCES plans(plan),
     name VARCHAR(255),
     lat FLOAT,
-    lon float
+    lon FLOAT
 );
 
-create table if not exists specializations (
-    id integer not null primary key,
+CREATE TABLE IF NOT EXISTS specializations (
+    id INTEGER NOT NULL PRIMARY KEY,
     name VARCHAR(255)
 );
 
-create table if not exists doctors (
-    id integer not null primary key,
-    minimum_plan integer references plans(plan),
+CREATE TABLE IF NOT EXISTS doctors (
+    id INTEGER NOT NULL PRIMARY KEY,
+    minimum_plan INTEGER REFERENCES plans(plan),
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     mail VARCHAR(255),
-    lat float,
-    lon float
-    );
+    lat FLOAT,
+    lon FLOAT
+);
 
-insert into plans("plan", "plan_name") values 
+INSERT INTO plans("plan", "plan_name") VALUES 
 (1, 'Plan 1'), 
 (2, 'Plan 2'), 
 (3, 'Plan 3');
 
-insert into users("dni", "plan", "first_name", "last_name", "password", "blocked", "mail") VALUES
+INSERT INTO users("dni", "plan", "first_name", "last_name", "password", "blocked", "mail") VALUES
 (1, 1, 'Diego', 'Armando', 'password', false, 'diego@mail.com'),
 (2, 2, 'Claudio', 'Paul', 'password', false, 'claudio@paul.com');
 
-insert into hospitals("id", "minimum_plan", "name", "lat", "lon") VALUES
+INSERT INTO hospitals("id", "minimum_plan", "name", "lat", "lon") VALUES
 (1, 1, 'hospital1', -33.0, -43.3),
-(2,
- 2,
- 'hospital2',
- -37.0,
- -53.3);
+(2, 2, 'hospital2', -37.0, -53.3);
