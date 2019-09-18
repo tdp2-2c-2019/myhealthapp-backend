1b4888e91099c680cc4228ff0ce321fa70f71b51
Manuel Porto <manu.porto94@gmail.com>
2019-09-18 17:18:37 -0300

    Encrypt and decrypt passwords (WIP) It has one unsolved bug (undefined reference)

--- src/server/services/users.js
+++ src/server/services/users.js
@@ -1,12 +1,14 @@
 import { db } from '../db';
-import { UserNotFoundError } from '../errors/errors';
+import { UserNotFoundError, ValidationError } from '../errors/errors';
+import CryptoService from '../utils/crypto';
 
 class UserService {
   static createUser(dni, password, mail) {
     return new Promise((resolve, reject) => {
       // TODO: Use bcrypt to store passwords and retrieve them
+      const hashedPassword = CryptoService.encrypt(password);
       db('users').where('dni', dni).update({
-        password,
+        password: hashedPassword,
         mail
       }).returning('dni')
         .then((res) => {
@@ -21,12 +23,18 @@ class UserService {
 
   static getUserByDniAndPassword(dni, password) {
     // TODO: Use bcrypt to store passwords and retrieve them
-    db.select().from('users').where({ dni, password })
+    db.select().from('users').where(dni)
       .then((rows) => {
         if (rows.length === 0) {
           throw new UserNotFoundError('User not found');
         } else {
-          return rows[0];
+          console.log(rows[0]);
+          const passwordsMatch = CryptoService.compare(password, rows[0].password);
+          if (!passwordsMatch) {
+            throw new ValidationError('Incorrect DNI or password.');
+          } else {
+            return rows[0];
+          }
         }
       });
   }
