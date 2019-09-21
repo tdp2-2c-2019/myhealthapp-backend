3b7852cfe068b5e4acb113948581f207971fa04b
Martín Stancanelli <mastanca14@gmail.com>
2019-09-21 19:22:20 -0300

    filter hospitals and doctors by name, tel and distance

--- src/server/health/health_services.js
+++ src/server/health/health_services.js
@@ -1,9 +1,30 @@
 import HealthService from '../services/health_services';
+import calculateDistance from '../utils/geodistance';
 
 const router = require('express').Router();
 
+const maxDistance = 0.5;
+
 router.get('/hospitals', (req, res, next) => {
-  HealthService.getHospitals()
+  const {
+    name, telephone, distance
+  } = req.query;
+  const origin = {
+    lat: Number(req.query.originLat),
+    lon: Number(req.query.originLon)
+  };
+  HealthService.getHospitals().filter((hospital) => {
+    const destination = {
+      lat: Number(hospital.lat),
+      lon: Number(hospital.lon)
+    };
+    const nameFilter = !name || (name && hospital.name.localeCompare(name) === 0);
+    const telephoneFilter = !telephone || (telephone
+      && hospital.telephone.localeCompare(telephone) === 0);
+    const distanceFilter = (!distance && calculateDistance(origin, destination) < maxDistance)
+      || (distance && calculateDistance(origin, destination) < distance);
+    return nameFilter && telephoneFilter && distanceFilter;
+  })
     .then(hospitals => res.status(200).send(hospitals)).catch(err => next(err));
 });
 
@@ -18,7 +39,25 @@ router.get('/doctors/:id', (req, res, next) => {
 });
 
 router.get('/doctors', (req, res, next) => {
-  HealthService.getDoctors().then(doctors => res.status(200).send(doctors)).catch(err => next(err));
+  const {
+    name, telephone, distance
+  } = req.query;
+  const origin = {
+    lat: Number(req.query.originLat),
+    lon: Number(req.query.originLon)
+  };
+  HealthService.getDoctors().filter((doctor) => {
+    const destination = {
+      lat: Number(doctor.lat),
+      lon: Number(doctor.lon)
+    };
+    const nameFilter = !name || (name && doctor.name.localeCompare(name) === 0);
+    const telephoneFilter = !telephone || (telephone
+      && doctor.telephone.localeCompare(telephone) === 0);
+    const distanceFilter = (!distance && calculateDistance(origin, destination) < maxDistance)
+      || (distance && calculateDistance(origin, destination) < distance);
+    return nameFilter && telephoneFilter && distanceFilter;
+  }).then(doctors => res.status(200).send(doctors)).catch(err => next(err));
 });
 
 router.get('/', (req, res, next) => {
