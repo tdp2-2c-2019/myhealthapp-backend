aeade1fbc86a07ddfaf6fce7cc53213b4a60826d
Martín Stancanelli <mastanca14@gmail.com>
2019-09-21 19:32:59 -0300

    Fix import typo

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const HandlerGenerator = require('./middleware/jwt');
+const { HandlerGenerator } = require('./middleware/jwt');
 
 const app = express();
 
