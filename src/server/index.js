2ec4944b352559b209ac4d8680dfcfc6ac5e5292
Manuel Porto <manu.porto94@gmail.com>
2019-09-22 19:31:48 -0300

    Check auth token for health services routes

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const { HandlerGenerator } = require('./middleware/jwt');
+const { checkToken, HandlerGenerator } = require('./middleware/jwt');
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-app.use('/api/health-services', require('./health/health_services').default);
+app.use('/api/health-services', checkToken, require('./health/health_services').default);
 
 app.use('/api/users', require('./users/users').default);
 
