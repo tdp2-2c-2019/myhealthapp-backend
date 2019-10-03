bda95e089a5d70e8c65acf6353d5b2789f6857c7
Manuel Porto <manu.porto94@gmail.com>
2019-10-03 13:03:30 -0300

    Add CORS

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const { HandlerGenerator } = require('./middleware/jwt');
 
 const app = express();
 
+app.use(cors());
 app.use(express.static('dist'));
 app.use(express.json());
 app.use(express.urlencoded({
