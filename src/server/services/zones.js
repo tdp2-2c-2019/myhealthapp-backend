5edd2ce780fb73d7a0a6c05048a8a9811122a956
Martín Stancanelli <mastanca14@gmail.com>
2019-10-14 15:26:01 -0300

    Fix union knex syntax

--- src/server/services/zones.js
+++ src/server/services/zones.js
@@ -3,7 +3,7 @@ import { db } from '../db';
 class ZoneService {
   static getZones() {
     return new Promise((resolve, reject) => {
-      db.select('zone').from('doctors').union(() => { db.select('zone').from('hospitals'); })
+      db.select('zone').from('doctors').whereNotNull('zone').union([db.select('zone').from('hospitals').whereNotNull('zone')])
         .then(rows => resolve(rows))
         .catch(() => reject(new Error('Ocurrió un error al obtener las zonas')));
     });
