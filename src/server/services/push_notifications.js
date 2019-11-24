0c83d13a1eb7374df27efcc01b675005a0f67ae2
Manuel Porto <manuel.porto@mercadolibre.com>
2019-11-24 08:50:22 -0300

    Read conf file from an env variable

--- src/server/services/push_notifications.js
+++ src/server/services/push_notifications.js
@@ -1,11 +1,17 @@
 
 const admin = require('firebase-admin');
-const serviceAccount = require('../../../keys/serviceAccountKey.json');
+
+function getGoogleCredentials() {
+  const googleCredentials = Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS_B64, 'base64').toString();
+  const jsonGCredentials = JSON.parse(googleCredentials);
+  return jsonGCredentials;
+}
 
 class PushNotificationsService {
   constructor() {
+    const googleCredentials = getGoogleCredentials();
     this.app = admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert(googleCredentials),
       databaseURL: 'https://myhealthapp-255602.firebaseio.com'
     });
     // this.messaging = admin.messaging(this.app);
